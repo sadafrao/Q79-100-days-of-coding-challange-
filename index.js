71d7bb896a52e89ad2ebbd89f7914e90da0ed86a
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-14 21:54:47 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// Q79
+// this ste show a car object with detail
+var Car = {
+    make: "Toyota",
+    model: "Camry",
+    year: 2020,
+};
+console.log(Car.model); // we use dot to get the model of a car
---
+++ index.ts
@@ -0,0 +1,9 @@
+// Q79
+// this ste show a car object with detail
+
+let Car = {
+    make: "Toyota",
+    model: "Camry",
+    year: 2020,
+};
+console.log(Car.model);// we use dot to get the model of a car
